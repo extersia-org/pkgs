ef9a8017e0a11eb8b568769be962a275735503ef
isabel <isabel@isabelroses.com>
2026-05-16 11:14:55 +0100

    blahaj: 0-unstable-2026-04-25 -> 0-unstable-2026-05-15
    Diff: https://github.com/isabelroses/blahaj/compare/29468536965b87d26278a7a230c968714d7ec9dc...a69af3a2ebdba3e7d4a4327eae829bebba96a59b

--- pkgs/bl/blahaj/package.nix
+++ pkgs/bl/blahaj/package.nix
@@ -8,16 +8,16 @@
 }:
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "blahaj";
-  version = "0-unstable-2026-04-25";
+  version = "0-unstable-2026-05-15";
 
   src = fetchFromGitHub {
     owner = "isabelroses";
     repo = "blahaj";
-    rev = "29468536965b87d26278a7a230c968714d7ec9dc";
-    hash = "sha256-BGntI8Fe3fgtnXMX7SiGbHDqvo/FX5XMcaJbd/y2wo8=";
+    rev = "a69af3a2ebdba3e7d4a4327eae829bebba96a59b";
+    hash = "sha256-uh0KCi/uYkvr6id582En0EWqsgTvSvisuCu/M/uMKs4=";
   };
 
-  cargoHash = "sha256-gNSd+5RdBVjY0YZJhwDmQgfC/0vllIetFufqsGbqyeY=";
+  cargoHash = "sha256-iagcIsjFj2k0HBKAAi2n6YTkhUA9BLv4QxsCziYULBU=";
 
   buildInputs = [ openssl ];
   nativeBuildInputs = [ pkg-config ];
