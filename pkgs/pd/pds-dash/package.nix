3699137a237566ce4528d6b053f4440bf0a558ff
isabel <isabel@isabelroses.com>
2026-05-26 17:27:22 +0100

    pds-dash: update

--- pkgs/pd/pds-dash/package.nix
+++ pkgs/pd/pds-dash/package.nix
@@ -1,61 +1,21 @@
 {
   lib,
-  pnpm,
-  stdenvNoCC,
-  makeWrapper,
-  nodejs-slim,
-  fetchPnpmDeps,
-  pnpmConfigHook,
+  rustPlatform,
   fetchFromGitHub,
   nix-update-script,
 }:
-stdenvNoCC.mkDerivation (finalAttrs: {
+rustPlatform.buildRustPackage (finalAttrs: {
   pname = "pds-dash";
-  version = "0-unstable-2026-05-15";
+  version = "0-unstable-2026-05-26";
 
   src = fetchFromGitHub {
     owner = "tgirlcloud";
     repo = "pds-dash";
-    rev = "389747ea2c048f0f97c9299b3d7e3a1bdbe8a423";
-    hash = "sha256-whp1+anTbUtaAGKG2/hZj3C1+iXf11f9QGIm0bKvQoI=";
+    rev = "e5b57a6d47d9986685db799dd361c23695362ab4";
+    hash = "sha256-Jl/aYMrmlkWnWt9HLOqdxdcuAAAChukIa8LvIh9fwQ0=";
   };
 
-  nativeBuildInputs = [
-    pnpm
-    nodejs-slim
-    makeWrapper
-    pnpmConfigHook
-  ];
-
-  pnpmDeps = fetchPnpmDeps {
-    inherit (finalAttrs) pname version src;
-    hash = "sha256-8zNV4Dy9XvuzvRPQbjitRXAEhJPoJCnbz1X2+9UV0PI=";
-    fetcherVersion = 3;
-  };
-
-  env.ASTRO_TELEMETRY_DISABLED = 1;
-
-  buildPhase = ''
-    runHook preBuild
-
-    pnpm run build
-
-    runHook postBuild
-  '';
-
-  installPhase = ''
-    runHook preInstall
-
-    mkdir -p $out
-
-    cp -r node_modules $out/node_modules
-    cp -r dist/* $out
-
-    makeWrapper ${nodejs-slim}/bin/node $out/bin/pds-dash \
-      --add-flags "$out/server/entry.mjs"
-
-    runHook postInstall
-  '';
+  cargoHash = "sha256-nt8QLciQyXvvFI3UpcKEkwGSZe7BiabiSBtpNo43m3M=";
 
   passthru.updateScript = nix-update-script {
     extraArgs = [
