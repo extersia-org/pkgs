16f8943890a47f3245a32ad0fe6833dc2e9abfbc
isabel <isabel@isabelroses.com>
2026-05-26 08:31:15 +0100

    hostling: 0-unstable-2026-05-21 -> 0-unstable-2026-05-25
    Diff: https://github.com/BatteredBunny/hostling/compare/721657f1b00916d1423ff33712a377ea96edb383...a816810580bc2fd0ed84a1e2b5a79ae1db6385af

--- pkgs/ho/hostling/package.nix
+++ pkgs/ho/hostling/package.nix
@@ -7,13 +7,13 @@
 }:
 buildGoModule {
   pname = "hostling";
-  version = "0-unstable-2026-05-21";
+  version = "0-unstable-2026-05-25";
 
   src = fetchFromGitHub {
     owner = "BatteredBunny";
     repo = "hostling";
-    rev = "721657f1b00916d1423ff33712a377ea96edb383";
-    hash = "sha256-njOK2rMaxmGq2KGR8WlbIFgA0+EMeO2/WqKWzhGFz7Q=";
+    rev = "a816810580bc2fd0ed84a1e2b5a79ae1db6385af";
+    hash = "sha256-ht07dQVq7eRz2wI/OTUfNqS09wOUrnQHLSHdb/sNVSs=";
   };
 
   vendorHash = "sha256-ZLozZWXhbqHMGslNFgA0YYGy0YvxD124A4ONSmbU49c=";
