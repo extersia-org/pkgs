104d35750732d6b1193070fab75eb7418a4e9097
isabel <isabel@isabelroses.com>
2026-06-15 15:49:45 +0100

    packages.hostling-frontend: 0.4.0-unstable-2026-06-03 -> 0.4.0-unstable-2026-06-10
    Diff: https://github.com/BatteredBunny/hostling/compare/9b5266709b3c83d1229472482573cb5455dd552d...5842955cb7e186605532abd9004ea40c02b606cf

--- pkgs/ho/hostling-frontend/package.nix
+++ pkgs/ho/hostling-frontend/package.nix
@@ -13,13 +13,13 @@ let
 in
 stdenv.mkDerivation (finalAttrs: {
   pname = "hostling-frontend";
-  version = "0.4.0-unstable-2026-06-03";
+  version = "0.4.0-unstable-2026-06-10";
 
   src = fetchFromGitHub {
     owner = "BatteredBunny";
     repo = "hostling";
-    rev = "9b5266709b3c83d1229472482573cb5455dd552d";
-    hash = "sha256-J2FouUwNLu7pn6xRxeK7yurocZJhDBRRah8qtgm36xM=";
+    rev = "5842955cb7e186605532abd9004ea40c02b606cf";
+    hash = "sha256-tWbdh9ONP8V9J6yKAZ+4WITeRnZ08QpkwyhOy46iz8U=";
   };
 
   sourceRoot = "${finalAttrs.src.name}/frontend";
@@ -39,7 +39,7 @@ stdenv.mkDerivation (finalAttrs: {
       ;
     inherit pnpm;
     fetcherVersion = 3;
-    hash = "sha256-+fcXIOPv3zKqQg5Rg56f8NNHyxlJGe2/aq5WJ+ERS7g=";
+    hash = "sha256-Haut9cD8AACYrw7SVeXv24Z5w/c4rlckYaQgiWyET60=";
   };
 
   buildPhase = ''
