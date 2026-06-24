2670e5e4bb8b5851bb7f0632444e187b48d6fda2
isabel <isabel@isabelroses.com>
2026-06-24 10:07:39 +0100

    packages.hostling: 0.4.0-unstable-2026-06-10 -> 0.4.0-unstable-2026-06-21
    Diff: https://github.com/BatteredBunny/hostling/compare/5842955cb7e186605532abd9004ea40c02b606cf...14616b43c25fc0ed9add935d0f11bd41c847c242

--- pkgs/ho/hostling/package.nix
+++ pkgs/ho/hostling/package.nix
@@ -7,16 +7,16 @@
 }:
 buildGoModule {
   pname = "hostling";
-  version = "0.4.0-unstable-2026-06-10";
+  version = "0.4.0-unstable-2026-06-21";
 
   src = fetchFromGitHub {
     owner = "BatteredBunny";
     repo = "hostling";
-    rev = "5842955cb7e186605532abd9004ea40c02b606cf";
-    hash = "sha256-tWbdh9ONP8V9J6yKAZ+4WITeRnZ08QpkwyhOy46iz8U=";
+    rev = "14616b43c25fc0ed9add935d0f11bd41c847c242";
+    hash = "sha256-n2DH+2NLerfAziBbznYotSl2BwPLwIW1qivIwqWjK/4=";
   };
 
-  vendorHash = "sha256-wsZHXMom1C5d624nzyPorBqC5lKBtarN1copNOy7uDI=";
+  vendorHash = "sha256-9iRUJ8YzKscg++Kgu8KYKGsxzZ1aYhzBZCFcrfZodts=";
 
   prePatch = ''
     cp -r ${hostling-frontend} ./public/dist
